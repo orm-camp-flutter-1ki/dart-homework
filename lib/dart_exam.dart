a8fcd7b0ff0db608f303f7e57e594274d5738c9c
Dalso <flskwhdhk@gmail.com>
2023-12-12 16:07:14 +0900

    디버그 연습문제 - 2

--- lib/dart_exam.dart
+++ lib/dart_exam.dart
@@ -2,6 +2,11 @@ import 'dart_debug_sample.dart';
 
 void main() {
   YukymController yukymController = YukymController();
+
+  // DateFormat 이 잘못되어있음.
+  // 시간 변환 getTyB 쪽 if문 잘못되어있음.
+
+  print(yukymController.nowDate);
   print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
   print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
 }
