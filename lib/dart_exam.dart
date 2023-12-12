58cb4e86528bf6a3427da587969f6221e4a9493b
slee3593 <slee3593@gatech.edu>
2023-12-12 16:32:15 +0900

    DateFormat 형식 이슈 수정

--- lib/dart_exam.dart
+++ lib/dart_exam.dart
@@ -4,4 +4,9 @@ void main() {
   YukymController yukymController = YukymController();
   print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
   print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
+
+  // nowDate의 값이 잘 저장되는지 test - 값이 바뀌면 안 됨
+  print(yukymController.nowDate);
+
+
 }
