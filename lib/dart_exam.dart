5027792f6fb57de4210e295331a8fa6d95478e69
ssd72 <sungdae.yi@gmail.com>
2023-12-12 11:18:50 +0100

    12월 12일 디버깅 연습문제 2

--- lib/dart_exam.dart
+++ lib/dart_exam.dart
@@ -2,6 +2,7 @@ import 'dart_debug_sample.dart';
 
 void main() {
   YukymController yukymController = YukymController();
+  print(yukymController.nowDateTime);
   print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
   print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
 }
