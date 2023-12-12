151574521a6ba746a241aead948fa78502625e60
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-12 10:59:08 +0900

    문제 업로드

--- lib/dart_exam.dart
+++ lib/dart_exam.dart
@@ -1,3 +1,7 @@
-int calculate() {
-  return 6 * 7;
+import 'dart_debug_sample.dart';
+
+void main() {
+  YukymController yukymController = YukymController();
+  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
+  print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
 }
