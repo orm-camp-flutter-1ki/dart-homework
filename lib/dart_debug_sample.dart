3d30432f87bd5aa7e32cf26fb982d527c25e5fa6
YongYeon Kim <assavictory@gmail.com>
2023-12-13 11:42:01 +0900

    리턴값 불필요 공백 제거 ( 문제결과값과 형식 통일)

--- lib/dart_debug_sample.dart
+++ lib/dart_debug_sample.dart
@@ -7,7 +7,7 @@ class YukymController {
   late String nowTime;
 
   // 1. 자시의 국 : 갑자1국 = getTyOne()의 값
-  String getTyA() => '경오 ${(int.parse(nowDate.substring(5, 7)) / 2).ceil()}국';
+  String getTyA() => '경오${(int.parse(nowDate.substring(5, 7)) / 2).ceil()}국';
 
-  String getTyB() => '갑자 ${(DateTime.now().hour / 2).ceil()}국';
+  String getTyB() => '갑자${(DateTime.now().hour / 2).ceil()}국';
 }
